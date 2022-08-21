3be85256ac2b836ee094a2d6f9020ad4f76ac958
joymush <joy.chemutai@student.moringaschool.com>
2022-08-21 14:00:11 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 function addingEventListener() {
+    prompt('What have you done');
+    const input = document.getElementById("button");
+    input.addEventListener('click', addingEventListener);
 }
